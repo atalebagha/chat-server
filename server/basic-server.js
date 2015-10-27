992ee26677c0ab610c07a3530496df1e3df8651e
Abdul-Rahman Taleb-Agha <abdulrahman@talebagha.net>
2015-10-27 10:22:39 -0700

    small update to server

--- server/basic-server.js
+++ server/basic-server.js
@@ -2,6 +2,10 @@
 var http = require("http");
 var reqHandler = require("./request-handler.js");
 var express = require('express');
+var _ = require('underscore');
+var bodyParser = require('body-parser');
+var multer = require('multer'); // v1.0.5
+var upload = multer();
 
 
 var app = express();
@@ -12,29 +16,56 @@ var server = app.listen(3000, "127.0.0.1", function () {
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+  console.log('Chatterbox Server app listening at http://%s:%s', host, port);
 });
 
-app.use('/', router);
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true }))
 
-router.get('/classes/messages', function (req, res, next) {
+app.post('/send', function (req, res, next) {
+  //var data = JSON.parse(req.body);
+  console.log(req.body);
+  //data.createdAt = new Date();
+  //messages.push(data);
+  res.status(201);
+})
+
+
+app.get('/classes/messages', function (req, res, next) {
   res.status(200);
+  res.json({results: messages});
   next();
 });
 
-router.get('/log', function (req, res, next) {
+app.get('/log', function (req, res, next) {
   res.status(200);
-  res.send("Hello World");
+  res.send("This is an amazing Log!");
   next();
 });
 
-router.get('/classes/:room', function (req, res, next) {
+app.get('/classes/:room', function (req, res, next) {
+  //res.send('You are in room ' + req.params.room);
+  var filtered = filterMessagesByRoom(req.params.room);
+  res.json({results: filtered});
   res.status(200)
 });
 
+var messages = [
+  { roomname: 'lobby', username: 'abcd', text: 'How are you?', createdAt: 'Tue, 27 Oct 2015 14:54:07 GMT'},
+  { roomname: 'lobby', username: 'xyz', text: 'Great!', createdAt: 'Tue, 27 Oct 2015 14:54:13 GMT'},
+  { roomname: 'room1', username: 'xyz', text: 'How are you?', createdAt: 'Tue, 27 Oct 2015 14:54:18 GMT'},
+  { roomname: 'room1', username: 'abcd', text: 'Great!', createdAt: 'Tue, 27 Oct 2015 14:54:22 GMT'},
+  { roomname: 'room32', username: 'xyz', text: 'How are you?', createdAt: 'Tue, 27 Oct 2015 14:54:28 GMT'},
+  { roomname: 'room32', username: 'abcd', text: 'Tired of you asking!', createdAt: 'Tue, 27 Oct 2015 14:54:35 GMT'}
+];
 
-
-
+var filterMessagesByRoom = function (room) {
+  return _.filter(messages, function (val) {
+    if (val.roomname === room) {
+      return val;
+    }
+  })
+}
 
 // Every server needs to listen on a port with a unique number. The
 // standard port for HTTP servers is port 80, but that port is
